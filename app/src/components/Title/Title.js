76069726b18e0ca354064765d0023cee8fc65708
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-10 20:07:51 +0300

    Setup mock markup

--- app/src/components/Title/Title.js
+++ app/src/components/Title/Title.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default class Title extends React.Component {
     render() {
-        return (<h1>{this.props.title}</h1>);
+        return (<h1 className="display-3">{this.props.title}</h1>);
     }
 }
