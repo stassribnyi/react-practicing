6114556db4d5761170343949f91d966b49935e7a
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-10 17:50:56 +0300

    Compose multiple components

---
+++ app/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import WelcomeTitle from './Title/WelcomeTitle.js';
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <WelcomeTitle />
+        );
+    }
+}
---
+++ app/src/components/Title/WelcomeTitle.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class WelcomeTitle extends React.Component {
+    render() {
+        return (<h1>Welcome!</h1>);
+    }
+}
