8bef97fe9eb26fbdc908574101ed5ec86ef97c3c
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-11 13:46:10 +0300

    Rearange sources

--- app/src/main.js
+++ app/src/main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import Bootstrap from './vendor/bootstrap-without-jquery';
+
 import App from './components/App';
 
 ReactDOM.render(
