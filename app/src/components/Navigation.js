9cff59eb8c4d9493ea7524643885d784bfe97c84
Stas Sribnyi <stas.sribnyi@gmail.com>
2018-07-12 17:25:50 +0300

    Replace vendor functionality with own, fix css

--- app/src/components/Navigation.js
+++ app/src/components/Navigation.js
@@ -2,28 +2,46 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 export default class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            collapsed: true
+        };
+
+        this.toggleCollapse = this.toggleCollapse.bind(this);
+    }
+
+    toggleCollapse() {
+        this.setState({
+            collapsed: !this.state.collapsed
+        });
+    }
+
     render() {
+        const navClass = this.state.collapsed ? 'collapse' : '';
+
         return (
             <nav className="navbar navbar-inverse navbar-fixed-top" role="navigation">
                 <div className="container">
                     <div className="navbar-header">
-                        <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <button type="button" className="navbar-toggle" onClick={this.toggleCollapse}>
                             <span className="sr-only">Toggle navigation</span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                             <span className="icon-bar"></span>
                         </button>
                     </div>
-                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <div className={`navbar-collapse ${navClass}`}>
                         <ul className="nav navbar-nav">
                             <li>
-                                <NavLink to="/featured" activeClassName="active-menu">Featured</NavLink >
+                                <NavLink to="/featured" activeClassName="active-menu" onClick={this.toggleCollapse}>Featured</NavLink >
                             </li>
                             <li>
-                                <NavLink to="/archives" activeClassName="active-menu">Archives</NavLink >
+                                <NavLink to="/archives" activeClassName="active-menu" onClick={this.toggleCollapse}>Archives</NavLink >
                             </li>
                             <li>
-                                <NavLink to="/settings" activeClassName="active-menu">Settings</NavLink>
+                                <NavLink to="/settings" activeClassName="active-menu" onClick={this.toggleCollapse}>Settings</NavLink>
                             </li>
                         </ul>
                     </div>
